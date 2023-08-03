f056080026987647677528125783248123d54425
codingpartnerNB <141160678+codingpartnerNB@users.noreply.github.com>
2023-08-03 19:45:24 +0530

    tagName function

--- index.js
+++ index.js
@@ -101,3 +101,10 @@ items[2].style.backgroundColor = "green";
 for(let i=0;i<items.length;i++){
     items[i].style.fontWeight = "bold";
 }
+
+
+// Editing li by getElementsByTagName
+let li = document.getElementsByTagName("li");
+for(let i=0;i<li.length;i++){
+    li[i].style.color = "blue";
+}
