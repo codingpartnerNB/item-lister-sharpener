23d988d7fecbc6bc45af4e099ff8bee12a95e06e
codingpartnerNB <nehab5314@gmail.com>
2023-08-04 18:01:50 +0530

    Traversing the DOM

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 
 // Changing title
 
-console.log(document.title);
-document.title = "hola";
+// console.log(document.title);
+// document.title = "hola";
 
 
 // console.log(document.doctype);
@@ -42,15 +42,15 @@ document.title = "hola";
 
 // Setting border bottom color to black of header
 
-let header = document.getElementById('main-header');
-header.style.borderBottom = '3px solid #000';
+// let header = document.getElementById('main-header');
+// header.style.borderBottom = '3px solid #000';
 
 
 // Making Add items to bold and its color green
 
-let addItem = document.querySelector('.title');
-addItem.style.fontWeight = "bold";
-addItem.style.color = "green";
+// let addItem = document.querySelector('.title');
+// addItem.style.fontWeight = "bold";
+// addItem.style.color = "green";
 
 
 
@@ -87,46 +87,132 @@ addItem.style.color = "green";
 
 
 // Making even list's background color yellow
-let even = document.querySelectorAll('li:nth-child(even)');
-even.forEach(function(item){
-    item.style.backgroundColor = "yellow";
-});
+// let even = document.querySelectorAll('li:nth-child(even)');
+// even.forEach(function(item){
+//     item.style.backgroundColor = "yellow";
+// });
 
 
 // Making 3rd list item to green background
-let items = document.getElementsByClassName('list-group-item');
+// let items = document.getElementsByClassName('list-group-item');
 // items[2].style.backgroundColor = "green";
 // console.log(items);
 
 
 // Making all list items font bold
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight = "bold";
-}
+// for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight = "bold";
+// }
 
 
 // Editing li by getElementsByTagName
-let li = document.getElementsByTagName("li");
-for(let i=0;i<li.length;i++){
-    li[i].style.color = "blue";
-}
+// let li = document.getElementsByTagName("li");
+// for(let i=0;i<li.length;i++){
+//     li[i].style.color = "blue";
+// }
 // console.log(li);
 
 // Making 2nd list item background color to green
 // let item2 = document.querySelector('li:nth-child(2)');
 // item2.style.backgroundColor = "green";
 // Making 3rd list item invisible
-let item3 = document.querySelector('li:nth-child(3)');
+// let item3 = document.querySelector('li:nth-child(3)');
 // item3.style.display = "none";
-item3.style.visibility = "hidden";
+// item3.style.visibility = "hidden";
 
 // Using querySelectorAll changing font color green of 2nd list item
-let listItem2 = document.querySelectorAll("li");
-listItem2[1].style.color = "green";
+// let listItem = document.querySelectorAll("li");
+// listItem[1].style.color = "green";
 
 
 // Making odd list's background color green
-let odd = document.querySelectorAll('li:nth-child(odd)');
-odd.forEach(function(item){
-    item.style.backgroundColor = "green";
-});
+// let odd = document.querySelectorAll('li:nth-child(odd)');
+// odd.forEach(function(item){
+//     item.style.backgroundColor = "green";
+// });
+
+
+
+// Traversing the DOM
+let node = document.querySelector('#items');
+//parentNode
+// console.log(node.parentNode);
+node.parentNode.style.backgroundColor="cyan";
+// console.log(node.parentNode.parentNode);
+
+//parentElement
+// console.log(node.parentElement);
+// node.parentElement.style.backgroundColor="cyan";
+// console.log(node.parentElement.parentElement);
+
+//childNodes
+// console.log(node.childNodes);
+ 
+//children
+// console.log(node.children);
+// console.log(node.children[2]);
+node.children[2].style.backgroundColor = "pink";
+
+//firstChild
+// console.log(node.firstChild);
+
+//firstElementChild
+// console.log(node.firstElementChild);
+
+//lastChild
+// console.log(node.lastChild);
+
+//lastElementChild
+// console.log(node.lastElementChild);
+// node.lastElementChild.textContent = "Hello";
+
+//nextSibling
+// console.log(node.nextSibling);
+
+//nextElementSibling
+// console.log(node.nextElementSibling);
+
+//previousSibling
+// console.log(node.previousSibling);
+
+//previousElementSibling
+// console.log(node.previousElementSibling);
+node.previousElementSibling.style.color = "green";
+
+
+
+//Adding Hello World before Item Lister
+
+//createElement -> creating div here
+let newDiv = document.createElement('div');
+//Adding class
+newDiv.className = 'hello';
+//Adding id
+newDiv.id = 'myDiv';
+//Adding attribute
+newDiv.setAttribute('title','This is my div');
+newDiv.setAttribute('value','Another div');
+
+//creating text node
+let newDivText = document.createTextNode('Hello World');
+
+//Add text to div
+newDiv.appendChild(newDivText);
+console.log(newDiv);
+
+
+//Adding element to DOM
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore(newDiv, h1);
+
+
+
+//Adding Hello World before Item 1
+let listItem1 = document.createElement('li');
+listItem1.className = 'list-group-item';
+let listItem1Text = document.createTextNode('Hello World');
+listItem1.appendChild(listItem1Text);
+let ul = document.querySelector('.list-group');
+let item1 = document.querySelector('.list-group-item');
+ul.insertBefore(listItem1,item1);
